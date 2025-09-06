468f4e995de9cffd912bd7425b25f8d88b43acb9
Matthew Trost <mrtrost@gmail.com>
2025-09-06 17:37:57 +0900

    test files

--- test/story.test.ts
+++ test/story.test.ts
@@ -7,13 +7,38 @@ loadEnv();
 
 const cwd = join(__dirname, "..");
 
+// Run ABCDE basic story
 execSync(
   `yarn ts ./run/autorun.ts \
-    --cartridgeDir ${quo(join(cwd, "test/fixtures/cartridges/teststory"))} \
-    --sessionPath ${quo(join(cwd, "test/fixtures/sessions/teststory-session.json"))} \
+    --cartridgeDir ${quo(join(cwd, "test/fixtures/cartridges/test01"))} \
+    --sessionPath ${quo(join(cwd, "test/fixtures/sessions/test01-session.json"))} \
     --openaiKey ${process.env.OPENAI_API_KEY} \
     --elevenlabsKey ${process.env.ELEVENLABS_API_KEY} \
     -i "input1" -i "invalid1" -i "invalid2" -i "Xylophone"
+    `,
+  { stdio: "inherit", cwd }
+);
+
+// Test intro
+execSync(
+  `yarn ts ./run/autorun.ts \
+    --cartridgeDir ${quo(join(cwd, "test/fixtures/cartridges/test02"))} \
+    --sessionPath ${quo(join(cwd, "test/fixtures/sessions/test02-session.json"))} \
+    --openaiKey ${process.env.OPENAI_API_KEY} \
+    --elevenlabsKey ${process.env.ELEVENLABS_API_KEY}
+    `,
+  { stdio: "inherit", cwd }
+);
+
+// Test resume
+execSync(
+  `yarn ts ./run/autorun.ts \
+    --cartridgeDir ${quo(join(cwd, "test/fixtures/cartridges/test02"))} \
+    --sessionPath ${quo(join(cwd, "test/fixtures/sessions/test02-session.json"))} \
+    --sessionResume \
+    --sessionTurn 2 \
+    --openaiKey ${process.env.OPENAI_API_KEY} \
+    --elevenlabsKey ${process.env.ELEVENLABS_API_KEY}
     `,
   { stdio: "inherit", cwd }
 );
